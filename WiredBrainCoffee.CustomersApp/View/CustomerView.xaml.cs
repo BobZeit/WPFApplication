f9282ea187be3102aa273df959a3a0d9462ebdc9
BobZeit <BobZeit@users.noreply.github.com>
2023-12-11 09:48:01 +0100

    initial commit

---
+++ WiredBrainCoffee.CustomersApp/View/CustomerView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WiredBrainCoffee.CustomersApp.View
+{
+    /// <summary>
+    /// Interaction logic for CustomerView.xaml
+    /// </summary>
+    public partial class CustomerView : UserControl
+    {
+        public CustomerView()
+        {
+            InitializeComponent();
+        }
+        private void CustomerView_Click(object sender, RoutedEventArgs e)
+        {
+            var currentColumn = (int)CustomerList.GetValue(Grid.ColumnProperty);
+            var newColumn = currentColumn == 2 ? 0 : 2;
+            CustomerList.SetValue(Grid.ColumnProperty, newColumn);
+           
+        }
+
+    }
+}
